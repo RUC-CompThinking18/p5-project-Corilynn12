362b14fcad0478a07d52204c998f06c7a2c42dfd
Cori Robinson <cr644@scarletmail.rutgers.edu>
2018-12-06 12:27:41 -0500

    Sunflowers

--- sketch.js
+++ sketch.js
@@ -1,7 +1,17 @@
+var value = 0
 function setup() {
-  // put setup code here
+  createCanvas(1920 , 1080);
+  img = loadImage("Sunflower.png");
 }
 
 function draw() {
-  // put drawing code here
+  background(100, 86, 35)
+  image(img, 0, 0);
+  fill(value)
+}
+function mouseDragged() {
+  value = value + 5;
+  if (value > 255) {
+    value = 0;
+  }
 }
